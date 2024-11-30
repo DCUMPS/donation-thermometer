fe6ba55af8a6969f563f6d1d20f75d7f517e7fa4
Jake Farrell <jake.farrell88@mail.dcu.ie>
2024-11-30 01:58:52 +0000

    added new donation tracker, fixed error generating image, cleaned up repo

--- main.py
+++ main.py
@@ -1,63 +1,116 @@
 #!/usr/bin/python3
 
-import requests 
-from bs4 import BeautifulSoup 
-from PIL import Image, ImageDraw, ImageFont
+import requests
 import datetime
 import time
 import random
-from thermometer_config import loop_times, url, output_file, border_color, mercury_color, font_path, font_size
+import re
+from bs4 import BeautifulSoup
+from PIL import Image, ImageDraw, ImageFont
+
+font_path = "./assets/fonts/GaretHeavy.ttf"
+image_path = "./assets/images/donation_thermometer.png"
+
+
+def get_donation_count():
+    URL = "https://www.idonate.ie/fundraiser/MediaProductionSociety12"
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0"}
+
+    data = {
+        'totalRaised': 0,
+        'targetAmount': 0
+    }
+
+    try:
+        r = requests.get(url=URL, headers=headers, timeout=10)
+        r.raise_for_status()
+        soup = BeautifulSoup(r.content, 'html5lib')
+
+        script_tag = soup.find('script', string=re.compile('totalRaised'))
+
+        js_content = script_tag.string if script_tag else ''
+
+        total_raised_match = js_content.replace('\\', '').split(',')
+
+        for item in total_raised_match:
+            if '"totalRaised":' in item:
+                data['totalRaised'] = int(
+                    float(item.split(':')[1].replace('"', '')))
+            if '"targetAmount":' in item:
+                data['targetAmount'] = int(
+                    float(item.split(':')[1].replace('"', '')))
+
+        return data
+
+    except (requests.RequestException, ValueError, IndexError, AttributeError) as e:
+        return data
+
 
 def create_donation_thermometer(goal, current_donation, image_width=400, image_height=700):
-  
     image = Image.new("RGBA", (image_width, image_height), (0, 0, 0, 0))
     draw = ImageDraw.Draw(image)
-
-    font = ImageFont.truetype(font_path, size=font_size)
+    font = ImageFont.truetype(font_path, size=50)
     border_width = 10
     fixed_image_height = 700
     thermometer_width = 90
-    draw.rectangle([(image_width // 2 - thermometer_width // 2, 250), (image_width // 2 + thermometer_width // 2, fixed_image_height - 100)], outline=border_color, width=border_width)
 
-    max_thermometer_height = image_height
-    bar_height = current_donation
-    if current_donation >= goal:
-        bar_height = goal
+    draw.rectangle([(image_width // 2 - thermometer_width // 2, 250),
+                    (image_width // 2 + thermometer_width // 2, fixed_image_height - 100)],
+                   outline=(255, 255, 255), width=border_width)
 
-    mercury_height = (int((bar_height / goal) * max_thermometer_height)) / 2
+    max_thermometer_height = fixed_image_height - 350
+    bar_height = min(current_donation, goal)
+    mercury_height = int((bar_height / goal) * max_thermometer_height)
 
-    mercury_top = fixed_image_height - 95 - mercury_height + border_width
     mercury_bottom = fixed_image_height - 105 - border_width
+    mercury_top = mercury_bottom - mercury_height
+
     mercury_left = image_width // 2 - thermometer_width // 2 + 5 + border_width
     mercury_right = image_width // 2 + thermometer_width // 2 - 5 - border_width
-    draw.rectangle([(mercury_left, mercury_top), (mercury_right, mercury_bottom)], fill=mercury_color)
+
+    if mercury_height > 0:
+        draw.rectangle([(mercury_left, mercury_top),
+                       (mercury_right, mercury_bottom)], fill=(149, 239, 185))
 
     text = f"{current_donation} / {goal}"
     text_bbox = draw.textbbox((0, 0), text, font=font)
     text_width = text_bbox[2] - text_bbox[0]
-    draw.text(((image_width - text_width) // 2, fixed_image_height - 70), text, font=font, fill=(255, 255, 255))
+    draw.text(((image_width - text_width) // 2, fixed_image_height - 70),
+              text, font=font, fill=(255, 255, 255))
 
     text = f"DONATION\nPROGRESS"
     text_bbox = draw.textbbox((0, 0), text, font=font)
     text_width = text_bbox[2] - text_bbox[0]
-    draw.text(((image_width - text_width) // 2, fixed_image_height - 600), text, font=font, fill=(255, 255, 255))
+    draw.text(((image_width - text_width) // 2, fixed_image_height - 600),
+              text, font=font, fill=(255, 255, 255))
 
     return image
 
-for i in range(loop_times):
-    URL = url
-    headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0"} 
-    r = requests.get(url=URL, headers=headers) 
-    soup = BeautifulSoup(r.content, 'html5lib')
-    current_donation = soup.find('div', attrs = {'class':'ifs-right-fundraisers-head'}) 
-    donation_target = soup.find('div', attrs = {'class':'support-cause'}) 
-    current_donation_amount = int(str(current_donation).split()[3].split("€")[1].split("<")[0].replace(",",""))
-    goal_amount = int(str(donation_target).split("€")[1].split("<")[0].replace(",",""))
-    thermometer_image = create_donation_thermometer(goal_amount, current_donation_amount)
-    thermometer_image.save(output_file)
-    cropped_image = Image.open(output_file)
-    cropped_image = cropped_image.crop((0, 110, 400, 700))
-    cropped_image.save(output_file)
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Donation Count Updated ({current_time}): €{current_donation_amount} out of €{goal_amount}")
-    time.sleep(random.randint(100, 300))
+
+while True:
+    try:
+        data = get_donation_count()
+        current_donation_amount = data['totalRaised']
+        goal_amount = data['targetAmount']
+
+        thermometer_image = create_donation_thermometer(
+            goal_amount, current_donation_amount)
+        thermometer_image.save(image_path)
+
+        cropped_image = Image.open(image_path)
+        cropped_image = cropped_image.crop((0, 110, 400, 700))
+        cropped_image.save(image_path)
+
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(
+            f"Donation Count Updated ({current_time}): €{current_donation_amount} out of €{goal_amount}")
+
+        time.sleep(random.randint(10, 60))
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        time.sleep(10)
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        time.sleep(10)
--- thermometer_config.py
+++
@@ -1,9 +0,0 @@
-# Config
-
-loop_times = 10000
-url = "https://www.idonate.ie/fundraiser/MediaProductionSociety11"
-output_file = "donation_thermometer.png"
-border_color = (255, 255, 255)
-mercury_color = (149, 239, 185)
-font_path = "GaretHeavy.ttf"
-font_size = 50
